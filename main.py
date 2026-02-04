d329d51ed2078bfbc614482e8c2eec43e5320512
mylat1488228 <ygokagay@gmail.com>
2026-02-05 00:07:15 +0300

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import asyncio
 import logging
+import matplotlib
+matplotlib.use('Agg')
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init_db
 from handlers import router
 
 async def main():
-    # Инициализация БД
     await init_db()
-    
-    # Настройка бота
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
-    
-    print("Бот запущен на aiogram 3!")
+    print("Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Бот остановлен")
+    try: asyncio.run(main())
+    except: pass
